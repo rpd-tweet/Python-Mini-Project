6d1f584fdcebad59735a5f2a95087dfbd4acbf86
rpdstranger <60440808+rpd-tweet@users.noreply.github.com>
2020-02-15 19:28:17 +0530

    Update label.py

--- demo project/label.py
+++ demo project/label.py
@@ -1,6 +1,11 @@
-import tkinter as tk
+import tkinter as tk   # tinker module containing tk toolkit 
+                       # renamed as tk
 
-root=tk.Tk()
-w=tk.Label(root,text="First Project!!")
-w.pack()
-root.mainloop()
+root=tk.Tk()  # root widget
+
+w=tk.Label(root,text="First Project!!") # label widget
+
+w.pack()  # packs the windows size to fit to the text
+
+root.mainloop() # event loop
+               # to make windows appear
